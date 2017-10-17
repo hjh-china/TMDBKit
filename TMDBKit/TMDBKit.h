39a1352f3f1db5896b9bf50425acb18ec26a4f97
Siyuan Cao <seeran@outlook.com>
2017-10-17 22:21:45 +0800

    Initial Commit

---
+++ TMDBKit/TMDBKit.h
@@ -0,0 +1,19 @@
+//
+//  TMDBKit.h
+//  TMDBKit
+//
+//  Created by Siyuan Cao on 2017/10/17.
+//  Copyright © 2017年 Siyuan Cao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TMDBKit.
+FOUNDATION_EXPORT double TMDBKitVersionNumber;
+
+//! Project version string for TMDBKit.
+FOUNDATION_EXPORT const unsigned char TMDBKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TMDBKit/PublicHeader.h>
+
+
