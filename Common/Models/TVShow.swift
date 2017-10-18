b22af5d5059406a81d537c146107fc29601ab1a0
Siyuan Cao <seeran@outlook.com>
2017-10-19 03:01:39 +0800

    Finished struct

--- Common/Models/TVShow.swift
+++ Common/Models/TVShow.swift
@@ -9,18 +9,45 @@
 import Foundation
 
 public struct TMDBTVShow: Codable {
-    let backdrop_path: String?
-    let firstAirDate: Date
-    let genreIds: [Int]
-    let id: Int
-    let originalLanguage: String
-    let originalName: String
-    let overview: String
-    let originCountry: [String]
-    let posterPath: String?
-    let popularity: Double
-    let name: String
-    let voteAverage: Double
-    let voteCount: Int
-    let rating: Int
+    public let backdropPath: String?
+    /// Raw **String** returned from TMDB, usually formatted as `YYYY-MM-dd`. Use `firstAirDate` property for **Date**.
+    public let rawFirstAirDateString: String?
+    public let genreIds: [Int]
+    public let id: Int
+    public let originalLanguage: String
+    public let originalName: String
+    public let overview: String?
+    public let originCountry: [String]
+    public let posterPath: String?
+    public let popularity: Double
+    public let name: String
+    public let voteAverage: Double
+    public let voteCount: Int
+    /// Only for getRatedTVShows()
+    public let rating: Int?
+    
+    public var firstAirDate: Date? {
+        if let raw = rawFirstAirDateString {
+            return raw.date(format: "YYYY-MM-dd")
+        } else {
+            return nil
+        }
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case posterPath = "poster_path"
+        case popularity
+        case id
+        case backdropPath = "backdrop_path"
+        case voteAverage = "vote_average"
+        case overview
+        case rawFirstAirDateString = "first_air_date"
+        case originCountry = "origin_country"
+        case genreIds = "genre_ids"
+        case originalLanguage = "original_language"
+        case voteCount = "vote_count"
+        case name
+        case originalName = "original_name"
+        case rating
+    }
 }
