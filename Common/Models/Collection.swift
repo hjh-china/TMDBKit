5f93683ba6e5798b36714dae74c436114e0faf44
Siyuan Cao <seeran@outlook.com>
2017-10-19 16:20:13 +0800

    Added TMDBCollection model

---
+++ Common/Models/Collection.swift
@@ -0,0 +1,27 @@
+//
+//  Collection.swift
+//  TMDBKit
+//
+//  Created by Siyuan Cao on 2017/10/19.
+//  Copyright © 2017年 Siyuan Cao. All rights reserved.
+//
+
+import Foundation
+
+public struct TMDBCollection: Codable {
+    let id: Int
+    let name: String
+    let overview: String
+    let posterPath: String?
+    let backdropPath: String?
+    let parts: [TMDBMovie]
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case overview
+        case posterPath = "poster_path"
+        case backdropPath = "backdrop_path"
+        case parts
+    }
+}
