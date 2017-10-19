5ac388a84902f6e3013fd10ab79cec251cc6c6a7
Siyuan Cao <seeran@outlook.com>
2017-10-19 16:20:33 +0800

    Added TMDBImage and  TMDBImages model

---
+++ Common/Models/Image.swift
@@ -0,0 +1,35 @@
+//
+//  Image.swift
+//  TMDBKit
+//
+//  Created by Siyuan Cao on 2017/10/19.
+//  Copyright © 2017年 Siyuan Cao. All rights reserved.
+//
+
+import Foundation
+
+public struct TMDBImage: Codable {
+    let aspectRatio: Double
+    let filePath: String
+    let height: Int
+    let language: String?
+    let voteAvrage: Double
+    let voteCount: Int
+    let width: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case aspectRatio = "aspect_ratio"
+        case filePath = "file_path"
+        case height
+        case language = "iso_639_1"
+        case voteAvrage = "vote_average"
+        case voteCount = "vote_count"
+        case width
+    }
+}
+
+public struct TMDBImages: Codable {
+    let id: Int
+    let backdrops: [TMDBImage]
+    let posters: [TMDBImage]
+}
