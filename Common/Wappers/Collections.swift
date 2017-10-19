d5fc0b0efe93eb86cb13aa410b1948d81708aaf0
Siyuan Cao <seeran@outlook.com>
2017-10-19 16:20:58 +0800

    Finished methods for Collections API

--- Common/Wappers/Collections.swift
+++ Common/Wappers/Collections.swift
@@ -11,6 +11,38 @@ import Foundation
 extension TMDBManager {
     /// [Collections API](https://developers.themoviedb.org/3/collections) wrapper class.
     public class CollectionsAPIWrapper {
+        /// Get collection details by id.
+        ///
+        /// - Parameters:
+        ///   - collectionId: Collection's ID.
+        ///   - language: Pass a ISO 639-1 value to display translated data for the fields that support it.
+        ///     - minLength: 2
+        ///     - pattern: ([a-z]{2})-([A-Z]{2})
+        ///     - default: en-US
+        ///   - completion: Completion handler.
+        public func getDetails(collectionId: Int, language: String? = nil, completion: @escaping (ObjectReturn<TMDBCollection>) -> ()) {
+            var query: [String: String] = [:]
+            if let language = language {
+                query["language"] = language
+            }
+            TMDBManager.shared.performRequest(path: "/collection/\(collectionId)", query: query, completion: completion)
+        }
         
+        /// Get the images for a collection by id.
+        ///
+        /// - Parameters:
+        ///   - collectionId: Collection's ID.
+        ///   - language: Pass a ISO 639-1 value to display translated data for the fields that support it.
+        ///     - minLength: 2
+        ///     - pattern: ([a-z]{2})-([A-Z]{2})
+        ///     - default: en-US
+        ///   - completion: Completion handler.
+        public func getImages(collectionId: Int, language: String? = nil, completion: @escaping (ObjectReturn<TMDBImages>) -> ()) {
+            var query: [String: String] = [:]
+            if let language = language {
+                query["language"] = language
+            }
+            TMDBManager.shared.performRequest(path: "/collection/\(collectionId)", query: query, completion: completion)
+        }
     }
 }
