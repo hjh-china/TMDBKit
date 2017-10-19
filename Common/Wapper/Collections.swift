50af3ca35d5b3aa1e2c951caa85eba50bbd8ad78
Siyuan Cao <seeran@outlook.com>
2017-10-19 09:49:42 +0800

    Added collections API

---
+++ Common/Wapper/Collections.swift
@@ -0,0 +1,14 @@
+//
+//  Collections.swift
+//  TMDBKit
+//
+//  Created by Siyuan Cao on 2017/10/19.
+//  Copyright © 2017年 Siyuan Cao. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - [Collections API](https://developers.themoviedb.org/3/collections)
+extension TMDBManager {
+    
+}
