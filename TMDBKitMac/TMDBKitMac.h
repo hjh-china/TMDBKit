b4055214785156bd391647c0323cbe4a41bbc64a
Siyuan Cao <seeran@outlook.com>
2017-10-21 01:28:32 +0800

    Add tvOS and macOS support

---
+++ TMDBKitMac/TMDBKitMac.h
@@ -0,0 +1,19 @@
+//
+//  TMDBKitMac.h
+//  TMDBKitMac
+//
+//  Created by Siyuan Cao on 2017/10/21.
+//  Copyright © 2017年 Siyuan Cao. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for TMDBKitMac.
+FOUNDATION_EXPORT double TMDBKitMacVersionNumber;
+
+//! Project version string for TMDBKitMac.
+FOUNDATION_EXPORT const unsigned char TMDBKitMacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TMDBKitMac/PublicHeader.h>
+
+
---
+++ TMDBKitTV/TMDBKitTV.h
@@ -0,0 +1,19 @@
+//
+//  TMDBKitTV.h
+//  TMDBKitTV
+//
+//  Created by Siyuan Cao on 2017/10/21.
+//  Copyright © 2017年 Siyuan Cao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TMDBKitTV.
+FOUNDATION_EXPORT double TMDBKitTVVersionNumber;
+
+//! Project version string for TMDBKitTV.
+FOUNDATION_EXPORT const unsigned char TMDBKitTVVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TMDBKitTV/PublicHeader.h>
+
+
