b76818c73c6d4c4fe342c199db978e856b3b4e81
Siyuan Cao <seeran@outlook.com>
2017-10-24 06:57:50 +0800

    General fix

--- Common/Wappers/Certifications.swift
+++ Common/Wappers/Certifications.swift
@@ -10,15 +10,13 @@ import Foundation
 
 extension TMDBManager {
     /// [Certifications API](https://developers.themoviedb.org/3/certifications) wrapper class.
-    public class CertificationsAPIWrapper: APIWrapper {        
+    public class CertificationsAPIWrapper: APIWrapper {
         /// Get an up to date list of the officially supported movie certifications on TMDb.
         ///
         /// - Parameter completion: Completion handler. If `.success`, carrys a value of
         /// `[String: [TMDBCertification]]`, of which key is country code, value is certifications for that country.
         public func getMovieCertifications(completion: @escaping (ObjectReturn<[String: [TMDBCertification]]>)-> Void) {
-            performRequest(path: "/certification/movie/list") { (result: ObjectReturn<[String: [String: [TMDBCertification]]]>) in
-                self.certificationsHelper(result, completion: completion)
-            }
+            certificationsHelper(0, completion: completion)
         }
         
         /// Get an up to date list of the officially supported TV show certifications on TMDb.
@@ -27,22 +25,23 @@ extension TMDBManager {
         /// `[String: [TMDBCertification]]`, of which key is country code, value is certifications
         /// for that country.
         public func getTVCertifications(completion: @escaping (ObjectReturn<[String: [TMDBCertification]]>)-> Void) {
-            performRequest(path: "/certification/tv/list") { (result: ObjectReturn<[String: [String: [TMDBCertification]]]>) in
-                self.certificationsHelper(result, completion: completion)
-            }
+            certificationsHelper(1, completion: completion)
         }
         
-        func certificationsHelper(_ raw: ObjectReturn<[String: [String: [TMDBCertification]]]>,
-                    completion: @escaping (ObjectReturn<[String: [TMDBCertification]]>)-> ()) {
-            switch raw {
-            case .success(let _certifications):
-                if let certifications = _certifications["certifications"] {
-                    completion(.success(object: certifications))
-                } else {
-                    completion(.fail(error: "Error getting certifications.".error(domain: "certifications")))
+        func certificationsHelper(_ flag: Int,
+                                  completion: @escaping (ObjectReturn<[String: [TMDBCertification]]>)-> ()) {
+            let path = flag == 0 ? "/certification/movie/list" : "/certification/tv/list"
+            performRequest(path: path) { (result: ObjectReturn<[String: [String: [TMDBCertification]]]>) in
+                switch result {
+                case .success(let _certifications):
+                    if let certifications = _certifications["certifications"] {
+                        completion(.success(object: certifications))
+                    } else {
+                        completion(.fail(error: "Error getting certifications.".error(domain: "certifications")))
+                    }
+                case .fail(let error):
+                    completion(.fail(error: error))
                 }
-            case .fail(let error):
-                completion(.fail(error: error))
             }
         }
     }
