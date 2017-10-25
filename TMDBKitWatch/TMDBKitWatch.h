4e703c74fa1e5d9f79ed7086240cb2cfa39bff7d
Siyuan Cao <seeran@outlook.com>
2017-10-25 18:59:28 +0800

    Add watchOS support

---
+++ TMDBKitWatch/TMDBKitWatch.h
@@ -0,0 +1,19 @@
+//
+//  TMDBKitWatch.h
+//  TMDBKitWatch
+//
+//  Created by Siyuan Cao on 2017/10/25.
+//  Copyright © 2017年 Siyuan Cao. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for TMDBKitWatch.
+FOUNDATION_EXPORT double TMDBKitWatchVersionNumber;
+
+//! Project version string for TMDBKitWatch.
+FOUNDATION_EXPORT const unsigned char TMDBKitWatchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TMDBKitWatch/PublicHeader.h>
+
+
